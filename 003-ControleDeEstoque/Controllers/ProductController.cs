11d587e6ba9150943c77ffdcc26b1c3e74e94c86
Jota Landes <jhonatamalta@hotmail.com>
2022-05-16 15:55:36 -0300

    003-controleDeEstoque CRUD finished

--- 003-ControleDeEstoque/Controllers/ProductController.cs
+++ 003-ControleDeEstoque/Controllers/ProductController.cs
@@ -20,7 +20,23 @@ namespace _003_ControleDeEstoque.Controllers
         {
             var Produtos = await context.produtos.ToListAsync();
             return Ok(Produtos);
-        }    
+        } 
+        [HttpGet("v1/product/{id:int}")]
+        public async Task<IActionResult> GetById(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context
+            )
+        {
+            var produto = await context.produtos.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+            
+        }
+
         [HttpPost("v1/product")]
         public ActionResult<Produto> Post(
             [FromBody] Produto produto,
@@ -35,6 +51,39 @@ namespace _003_ControleDeEstoque.Controllers
             context.produtos.Add(produto);
             context.SaveChanges();
             return produto;
+        }
+        [HttpPut("v1/product/{id:int}")]
+        public Produto Put(
+            [FromRoute]int id,
+            [FromBody] Produto produto,
+            [FromServices] AppDbContext context
+            )
+        {
+            var modelo = context.produtos.FirstOrDefault(x => x.Id == id);
+            if(modelo == null)
+            {
+                return produto;
+            }
+            modelo.Name = produto.Name;
+            modelo.shortDescription = produto.shortDescription;
+            modelo.Price = produto.Price;
+            modelo.Quantity = produto.Quantity;
+
+            context.produtos.Update(modelo);
+            context.SaveChanges();
+            return modelo;
+        }
+
+        [HttpDelete("v1/product/{id:int}")]
+        public async Task<Produto> Delete(
+             [FromRoute] int id,
+             [FromServices] AppDbContext context
+            )
+        {
+           var model = context.produtos.FirstOrDefault(x => x.Id == id);
+            context.produtos.Remove(model);
+            context.SaveChanges();
+            return model;
         }
     }
 }
